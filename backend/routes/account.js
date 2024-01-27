016f7e9102a48892d6aa7bb2a030e9a2d4bfd57f
nomanmaniyar <nomaanmaniyar123@gmail.com>
2024-01-27 18:42:51 +0530

    add - transfer amount

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -9,7 +9,29 @@ router.get("/balance", authMiddleware, async (req, res) => {
     res.json({ balance: account.balance })
 })
 
+router.post("/transfer", authMiddleware, async (req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
+    const { amount, to } = req.body;
+    console.log("TRANSFER CALLED:\nAMOUNT: " + amount + "\nTO: " + to);
+    const account = await Account.findOne({ userId: req.userId });
+
+    if (!account || account.balance < amount) {
+        await session.abortTransaction();
+        res.status(400).json({ message: "Insufficiant Fund/Balance." })
+    }
+    const toAccount = await Account.findOne({ userId: to });
+    if (!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({ message: "Invalid Account Number." })
+    }
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: amount*-1 } }).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
+    await session.commitTransaction();
+    res.json({ message: "Transfer Successfull." })
+})
 
 module.exports = router;
 
